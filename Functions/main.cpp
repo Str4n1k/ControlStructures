fd70947f7cc595638764f0dbb2c168bdab9e5999
Str4n1k <glebus.i@mail.ru>
2023-03-03 21:22:34 +0700

    update

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -6,9 +6,11 @@ using std::endl;
 
 int Sum(int a, int b); // Прототип функции – Объявление функции или же function declaration.
 int Dif(int a, int b);
-int Product(int a, int b);
+int Product(int a, int b = 1);
 double Quotient(double a, double b);
 int Reminder(int a, int b);
+int Factorial(int n);
+double Power(double a, int n);
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -20,7 +22,11 @@ void main()
 	cout << a << " - " << b << " = " << Dif(a,b) << endl;
 	cout << a << " * " << b << " = " << Product(a,b) << endl;
 	cout << a << " / " << b << " = " << Quotient(a,b) << endl;
-	cout << a << " % " << b << " = " << Reminder(a,b) << endl;
+	//cout << a << " % " << b << " = " << Reminder(a,b) << endl;
+	cout << a << " ^ " << b << " = " << Power(a, b) << endl;
+	int n;
+	cout << "Введите число для вычисления факториала: "; cin >> n;
+	cout << n << "! = " <<  Factorial(n) << endl;
 
 }
 int Sum(int a, int b)  // Реализация функции - Определение функции или же Function definition (
@@ -47,3 +53,26 @@ int Reminder(int a, int b)
 {
 	return a % b;
 }
+int Factorial(int n)
+{
+	int f = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		f *= i;
+	}
+	return f;
+}
+double Power(double a, int n)
+{
+	double N = 1;
+	if (n < 0)
+	{
+		a = 1 / a;
+		n = -n;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		N *= a;
+	}
+	return N;
+} 
---
+++ arrays/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n] = {};
+
+	cout << "Введите элемент массива: ";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	
+	// вывод массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
