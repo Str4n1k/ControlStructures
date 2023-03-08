39645dd07e0be347d3294b10933307551312f5f9
Str4n1k <glebus.i@mail.ru>
2023-03-09 01:56:13 +0700

    HomeWork

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -6,18 +6,34 @@ void main()
 	setlocale(LC_ALL, "");
 	const int n = 5;
 	int arr[n] = {};
+	int sum = 0;
+	int surplas = 0;
 
-	cout << "Введите элемент массива: ";
+
+	cout << "Введите элементы массива: ";
 	for (int i = 0; i < n; i++)
 	{
 		cin >> arr[i];
 	}
-	
+
 	// вывод массива на экран
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+
+	for (int i = 0; i < n; i++) //Сумма массива
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма массива равна: " << sum << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		surplas = sum / n;
+	}
+	cout << "Среднее арифметическое массива равна: " << surplas;
+
 }
 
