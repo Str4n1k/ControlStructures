747007a93e1224525148f56f2c59a65ef5032366
Str4n1k <glebus.i@mail.ru>
2023-04-09 20:06:13 +0700

    new

---
+++ FunctionTemplate/Maxmin.h
@@ -0,0 +1,6 @@
+#pragma once
+#include"Constants.h"
+template<typename T> T minValueIn(const T arr[], const int n);
+template<typename T> T minValueIn(const T arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T> T maxValueIn(const T arr[], const int n);
+template<typename T> T maxValueIn(const T arr[ROWS][COLS], const int ROWS, const int COLS);
--- FunctionTemplate/main.cpp
+++ FunctionTemplate/main.cpp
@@ -1,46 +1,37 @@
-#include<iostream>
-using namespace std;
-
-const int ROWS = 3;
-const int COLS = 4;
-
-void FillRand(int arr[], const int n);
-void FillRand(double arr[], const int n);
-void FillRand(int arr[ROWS][COLS], const int ROWS,const int COLS);
-
-template<typename T> void Print(const T arr[], const int n);
-template<typename T> void Print(const T arr[ROWS][COLS], const int ROWS,const int COLS);
-template<typename T> T Sum(const T arr[], const int n);
-template<typename T> double Avg(const T arr[], const int n);
-
-template<typename T> T minValueIn(const T arr[], const int n);
-template<typename T> T maxValueIn(const T arr[], const int n);
-
-template<typename T> void Sort(T arr[], const int n);
-
-template<typename T> void shiftLeft(T arr[], const int n, int number_of_shifts);
-template<typename T> void shiftRight(T arr[], const int n, int number_of_shifts);
+#include"stdafx.h"
+#include"Constants.h"
+#include"FillUniq.h"
+#include"Print.h"
+#include"Shifts.h"
+#include"Sum.h"
+#include"Avg.h"
+#include"Maxmin.h"
+#include"Sort.h"
+#include"Search.h"
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	
 	const int n = 10;
 	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	FillRand(arr, n);
+	UniqueRand(arr, n);
 	Print(arr, n);
 	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
 	cout << "Среднее арифметическое элементов массива: " << Avg(arr, n) << endl;
 	cout << "Минимальное значение в массиве:  " << minValueIn(arr, n) << endl;
 	cout << "Максимальное значение в массиве: " << maxValueIn(arr, n) << endl;
+	Search(arr, n);
 	Sort(arr, n);
 	Print(arr, n);
 
-	/*int number_of_shifts;
+	int number_of_shifts;
 	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
 	shiftLeft(arr, n, number_of_shifts);
 	Print(arr, n);
 	shiftRight(arr, n, number_of_shifts);
-	Print(arr, n);*/
+	Print(arr, n);
 
 	const int SIZE = 8;
 	double d_arr[SIZE];
@@ -53,142 +44,61 @@ void main()
 	Sort(d_arr, SIZE);
 	Print(d_arr, SIZE);
 	
-	int number_of_shifts;
 	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
 	shiftLeft(d_arr, SIZE, number_of_shifts);
 	Print(d_arr, SIZE);
 
 	int i_arr_2[ROWS][COLS];
+	double d_arr_2[ROWS][COLS];
+	char c_arr_2[ROWS][COLS];
 	FillRand(i_arr_2, ROWS, COLS);
+	FillRand(d_arr_2, ROWS, COLS);
+	FillRand(c_arr_2, ROWS, COLS);
+	cout << "Сгенерированные массивы" << endl;
 	Print(i_arr_2, ROWS, COLS);
-}
-void FillRand(int arr[], const int n)
-{
-	//rand();	//возвращает псевдослучайное число в диапазоне от 0 до 32 767 (MAX_RAND)
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-	}
-}
-void FillRand(double arr[], const int n)
-{
-	//rand();	//возвращает псевдослучайное число в диапазоне от 0 до 32 767 (MAX_RAND)
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 10000;
-		arr[i] /= 100;
-	}
-}
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % 100;
-		}
-	}
-}
+	Print(d_arr_2, ROWS, COLS);
+	Print(c_arr_2, ROWS, COLS);
 
-template<typename T> void Print(const T arr[], const int n)
-{
-	//Вывод массива на экран:
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-template<typename T> void Print(const T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
+	cout << "Сумма элементов массива: " << Sum(i_arr_2, ROWS,COLS) << endl;
+	cout << "Сумма элементов массива: " << Sum(d_arr_2, ROWS,COLS) << endl;
+	cout << "Сумма элементов массива: " << Sum(c_arr_2, ROWS,COLS) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(i_arr_2,ROWS,COLS) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(d_arr_2,ROWS,COLS) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(c_arr_2,ROWS,COLS) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(i_arr_2,ROWS,COLS) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(d_arr_2,ROWS,COLS) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(c_arr_2,ROWS,COLS) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(i_arr_2,ROWS,COLS) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(d_arr_2,ROWS,COLS) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(c_arr_2,ROWS,COLS) << endl;
+	cout << "Введите количество сдвигов в лево: "; cin >> number_of_shifts;
+	shiftLeft(i_arr_2, ROWS, COLS, number_of_shifts);
+	Print(i_arr_2, ROWS, COLS); cout << endl;
+	shiftLeft(d_arr_2, ROWS, COLS, number_of_shifts);
+	Print(d_arr_2, ROWS, COLS); cout << endl;
+	shiftLeft(c_arr_2, ROWS, COLS, number_of_shifts);
+	Print(c_arr_2, ROWS, COLS); cout << endl;
+	cout << "Введите количество сдвигов вправо: "; cin >> number_of_shifts;
+	shiftRight(i_arr_2, ROWS, COLS, number_of_shifts);
+	Print(i_arr_2, ROWS, COLS); cout << endl;
+	shiftRight(d_arr_2, ROWS, COLS, number_of_shifts);
+	Print(d_arr_2, ROWS, COLS); cout << endl;
+	shiftRight(c_arr_2, ROWS, COLS, number_of_shifts);
+	Print(c_arr_2, ROWS, COLS); cout << endl;
+	cout << "Сортировочка" << endl;
+	Sort(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS); cout << endl;
+	Sort(d_arr_2, ROWS, COLS);
+	Print(d_arr_2, ROWS, COLS); cout << endl;
+	Sort(c_arr_2, ROWS, COLS);
+	Print(c_arr_2, ROWS, COLS); cout << endl;
+	cout << "Поиск повторов " << endl;
+	Search(i_arr_2, ROWS, COLS); cout << endl;
+	Search(d_arr_2, ROWS, COLS); cout << endl;
+	Search(c_arr_2, ROWS, COLS); cout << endl;
 }
 
 
-template<typename T> T Sum(const T arr[], const int n)
-{
-	//Вычисление суммы элементов массива:
-	T sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sum += arr[i];
-	}
-	return sum;
-}
-template<typename T> double Avg(const T arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
 
-template<typename T> T minValueIn(const T arr[], const int n)
-{
-	T min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
-	}
-	return min;
-}
-template<typename T> T maxValueIn(const T arr[], const int n)
-{
-	T max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
-	return max;
-}
 
-template<typename T> void Sort(T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		//Счетчик 'i' выбирает элемент, в который нужно поместить минимальное значение из перебираемых
-		for (int j = i + 1; j < n; j++)
-		{
-			//Счетчик 'j' перебирает элементы, в поисках минимального
-			//arr[i] - выбранный элемент
-			//arr[j] - перебираемый элемент
-			if (arr[j] < arr[i])
-			{
-				T buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
 
-template<typename T> void shiftLeft(T arr[], const int n, int number_of_shifts)
-{
-	for (int i = 0; i < number_of_shifts; i++)
-	{
-		T buffer = arr[0];
-		for (int i = 0; i < n; i++)
-		{
-			arr[i] = arr[i + 1];
-		}
-		arr[n - 1] = buffer;
-	}
-}
-template<typename T> void shiftRight(T arr[], const int n, int number_of_shifts)
-{
-	/*for (int i = 0; i < number_of_shifts; i++)
-	{
-		int buffer = arr[n - 1];
-		for (int i = n - 1; i > 0; i--)
-		{
-			arr[i] = arr[i - 1];
-		}
-		arr[0] = buffer;
-	}*/
-
-	shiftLeft(arr, n, n - number_of_shifts);
-}
